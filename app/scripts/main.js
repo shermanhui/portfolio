cba0f937370c6e0be5c1d62ff073fe44fdc08686
shui91 <sherman.sy.hui@gmail.com>
2015-11-27 21:38:59 -0800

    feat: add header nav and footer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+function adjust_body_offset() {
+    $('body').css('padding-top', $('.navbar-inverse').outerHeight(true) + 'px' );
+}
+
+$(window).resize(adjust_body_offset);
+
+$(document).ready(adjust_body_offset);
+
+//https://jsfiddle.net/4c2yespy/8/
