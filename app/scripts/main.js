13ee88a51dcb3cb00fe9c5291dc84ba40d33d7d0
shui91 <sherman.sy.hui@gmail.com>
2015-11-28 23:22:56 -0800

    feat: text shows on hover, but using JS, looking for CSS only method

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,4 +6,23 @@ $(window).resize(adjust_body_offset);
 
 $(document).ready(adjust_body_offset);
 
+$(document).ready(function() {
+    $("[rel='tooltip']").tooltip();
+
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').fadeIn(250); //slideDown(250)
+        },
+        function(){
+            $(this).find('.caption').fadeOut(250); //.slideDown(205)
+        }
+    );
+
+    $(".disabled-link").hover(
+    	function() {
+    		$(this).find('.error').preventDefault();
+    	}
+    );
+});
+
 //https://jsfiddle.net/4c2yespy/8/
