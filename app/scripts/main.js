9fae8ccf08fa6895aca11ffa3d5a6f3235e058fd
shui91 <sherman.sy.hui@gmail.com>
2015-12-01 09:53:45 -0800

    chore: remove js that was moved into views

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,15 +10,6 @@ $(document).ready(function() {
 
 	$(document).ready(adjust_body_offset);
 
-    $('.thumbnail').hover(
-        function(){
-            $(this).find('.caption').fadeIn(250); //slideDown(250)
-        },
-        function(){
-            $(this).find('.caption').fadeOut(250); //.slideDown(205)
-        }
-    );
-
     $(".disabled-link").hover(
     	function() {
     		$(this).find('.error').preventDefault();
