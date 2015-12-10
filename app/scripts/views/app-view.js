f669646a3d171636d503baea33e6084887aa6c8f
shui91 <sherman.sy.hui@gmail.com>
2015-12-09 16:36:14 -0800

    chore: add console.log

--- app/scripts/views/app-view.js
+++ app/scripts/views/app-view.js
@@ -65,6 +65,7 @@ app.portfolioView = Backbone.View.extend({
 	},
 
 	render: function(){
+		console.log(this.collection.toJSON());
 		this.$el.html(this.projectTemplate({collection: this.collection.toJSON()}));
 
 		return this;
